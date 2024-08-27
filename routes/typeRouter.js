8362f5e2014909d4ffc08a1b44694f82f171075f
Steddy <qwesteddyoff@gmail.com>
2024-08-27 21:30:01 +0300

    fixef routes

--- routes/typeRouter.js
+++ routes/typeRouter.js
@@ -1,7 +1,9 @@
 const Router = require("express");
 const router = new Router();
 
-router.post("/");
-router.get("/");
+const typeController = require("../controllers/typeController");
+
+router.post("/", typeController);
+router.get("/", typeController);
 
 module.exports = router;
